28c7c1457dc4b3fa61e0ec510410abaa0eac8c62
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-11-07 09:37:01 -0500

    test: add test w/ local modomics

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,3 +11,14 @@ def test_cli():
     is_success = mimseq_out.returncode == 0
     # TODO: check md5sums of output files
     assert is_success
+
+def test_cli_local_modomics():
+    mimseq_out = subprocess.run(
+        "mimseq --species Hsap --cluster-id 0.97 --threads 2 --min-cov 0.0005 --max-mismatches 0.075 --control-condition HEK293T -n hg38_test --out-dir hg38_HEK239vsK562 --max-multi 4 --remap --remap-mismatches 0.05 --local-modomics sampleData_HEKvsK562.txt",
+        shell=True,
+        text=True,
+        capture_output=True,
+    )
+    is_success = mimseq_out.returncode == 0
+    # TODO: check md5sums of output files
+    assert is_success
